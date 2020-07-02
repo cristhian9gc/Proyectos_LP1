0136d93a5d727e68ca4e7a14ed0c7a54c0bf6a38
Cristian Gamboa <cristian9gc@gmail.com>
2020-07-02 17:19:47 -0500

    prog01 - prog02 - prog03 -prog04 listo

---
+++ src/estructuras/prog04.java
@@ -0,0 +1,14 @@
+
+package estructuras;
+
+public class prog04 {
+    public static void main(String[] args) {
+
+    int num = 1;
+    String mensaje = "Cristo viene pronto - Prepárate";
+    while(num<=10) {
+        System.out.println(mensaje);
+        num++;
+    }
+    }
+}
