5e1ffe0fc538f32d0ff72a31bf5ec23d6499c921
Cristian Gamboa <cristian9gc@gmail.com>
2020-07-07 14:35:38 -0500

    Prog 08 - metodo con parametros ahora con factorial

--- src/modularidad/prog08.java
+++ src/modularidad/prog08.java
@@ -11,8 +11,17 @@ public class prog08 {
     public static void saludo2(String nombre, String carrera){
         System.out.println(nombre+" Disfruta tu estadia en la carrera de "+ carrera);
     }
+    public static void factorial(int num){
+        int fact =1;
+        for(int i=1; i<=num; i++){
+            fact*=i;
+        }
+        System.out.println("Factorial de "+num+" es: "+ fact);
+    }
+    
     public static void main(String[] args) {
         saludo("Cristian");
         saludo2("Pamela Greta", "Ing. Sistemas");
+        factorial(5);
     }
 }
